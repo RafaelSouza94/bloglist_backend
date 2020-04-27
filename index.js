411e19f0582b6f0cec202a969827b7987ffc4e24
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-27 12:23:59 -0700

    external envs working

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require("./utils/config")
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -13,7 +14,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb+srv://fullstack:8nj0kJ3STX213KQJ@cluster0-a3khb.mongodb.net/bloglist-app?retryWrites=true&w=majority'
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
@@ -37,7 +38,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
