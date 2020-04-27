3ef727a59037782a363872bbc115ddeee306654f
RafaelSouza94 <rafaelrs57@hotmail.com>
2020-04-27 12:43:12 -0700

    add exercise 4.2

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require("./utils/config")
 const blogsRouter = require("./controllers/blogs")
+const logger = require("./utils/logger")
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {   // eslint-disable-line no-unused-vars
+        logger.info("Connected to MongoDB")
+    })
+    .catch((error) => {
+        logger.error("Error connecting to MongoDB: ", error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
