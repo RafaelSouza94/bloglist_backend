efffb2a59cb3b75659f1b448968d5e4f64035143
Rafael Souza <rafaelrochasouza@outlook.com.br>
2020-04-29 13:48:59 -0700

    add exercise 4.4

---
+++ utils/list_helper.js
@@ -0,0 +1,23 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item['likes']
+    }
+
+    return blogs.length === 0 
+        ? 0
+        : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+}
